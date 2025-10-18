a6fc14f27519eb99009cdfbcb124f8d102260327
Maxime ANANIVI <maximeananivi@192.168.1.67>
2025-10-18 23:15:57 +0000

    Love is blind

--- src/config/db.js
+++ src/config/db.js
@@ -26,7 +26,7 @@ export async function initDB() {
     await sql`
       CREATE TABLE IF NOT EXISTS transactions (
         id SERIAL PRIMARY KEY,
-        user_id VARCHAR(255) NOT NULL,
+        user_id INTEGER NOT NULL,
         title  VARCHAR(255) NOT NULL,
         amount  DECIMAL(10,2) NOT NULL,
         category VARCHAR(255) NOT NULL,
