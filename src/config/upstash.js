fcbd0227aae8deae92c0f0753836a80536d776ed
Maxime ANANIVI <maximeananivi@192.168.1.65>
2025-10-30 09:09:01 +0000

    Corrected Ratelimit

--- src/config/upstash.js
+++ src/config/upstash.js
@@ -5,7 +5,10 @@ import "dotenv/config";
 
 const ratelimit = new Ratelimit({
   redis: Redis.fromEnv(),
-  limiter: Ratelimit.slidingWindow(100, "60 s"),
+  // Augmenter les limites
+  limiter: Ratelimit.slidingWindow(500, "60 s"),
+  analytics: true,
+  prefix: "medremind-api",
 });
 
 export default ratelimit;
